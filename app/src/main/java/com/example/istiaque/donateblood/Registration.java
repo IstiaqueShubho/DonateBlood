a296907ec4a8d6b787ba2dbcb2adecb8bdc7e890
Istiaque Shubho <istiaquehossainshubho@gmail.com>
2019-03-03 01:04:41 +0600

    Third Commit

--- app/src/main/java/com/example/istiaque/donateblood/Registration.java
+++ app/src/main/java/com/example/istiaque/donateblood/Registration.java
@@ -30,7 +30,6 @@ public class Registration extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener authStateListener;
     private DatabaseReference firebaseDatabase;
 
-    private ProgressDialog progressDialog;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +39,6 @@ public class Registration extends AppCompatActivity {
 
         firebaseDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
 
-        progressDialog = new ProgressDialog(this);
 
         Name = (EditText) findViewById(R.id.name);
         Email = (EditText) findViewById(R.id.email);
@@ -67,21 +65,32 @@ public class Registration extends AppCompatActivity {
         String bloodgroup = Bloodgroup.getText().toString().trim();
         String location = Location.getText().toString().trim();
 
-        int date,month,year;
+        int date = 0,month = 0,year = 0;
         String date1 = Date.getText().toString().trim();
-        date = Integer.parseInt(date1);
+        if(!date1.isEmpty()){
+            date = Integer.parseInt(date1);
+            if(!(date >= 1 && date <= 31)){
+                date1 = "";
+                Toast.makeText(this,"Date Must be between 1 to 31",Toast.LENGTH_LONG).show();
+            }
+        }
         String month1 = Month.getText().toString().trim();
-        month = Integer.parseInt(month1);
+        if(!month1.isEmpty()){
+            month = Integer.parseInt(month1);
+            if(!(month >= 1 && month <= 12)){
+                month1 = "";
+                Toast.makeText(this,"Month Must be between 1 to 12",Toast.LENGTH_LONG).show();
+            }
+        }
         String year1 = Year.getText().toString().trim();
-        year = Integer.parseInt(year1);
+        if(!year1.isEmpty()){
+            year = Integer.parseInt(year1);
+        }
 
-        if(name.equals("") || email.equals("") ||  bloodgroup.equals("") && location.equals("") && date1.equals("") && month1.equals("") && year1.equals("")){
+        if(name.equals("") || email.equals("") ||  bloodgroup.equals("") || location.equals("") ||  date1.equals("") || month1.equals("") || year1.equals("")){
             Toast.makeText(this,"Fill all the areas with Valid Information.",Toast.LENGTH_LONG).show();
         }
         else{
-            progressDialog.setMessage("Registering User");
-            progressDialog.show();
-
             String user_id = firebaseDatabase.push().getKey();
 
             firebaseDatabase.child(user_id).child("Name").setValue(name);
@@ -92,8 +101,11 @@ public class Registration extends AppCompatActivity {
             firebaseDatabase.child(user_id).child("LastDonation").child("Month").setValue(month);
             firebaseDatabase.child(user_id).child("LastDonation").child("Year").setValue(year);
 
+            Toast.makeText(this,"Registration Successful.",Toast.LENGTH_LONG).show();
 
-            startActivity(new Intent(Registration.this,Userprofile.class));
+            Intent user = new Intent(Registration.this,Userprofile.class);
+            user.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(user);
         }
     }
 }
